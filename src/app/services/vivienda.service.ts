44a633c54589da107d7dae6eb600ab4ec305a7c1
vicentemarti31416 <vicentemarti31416@gmail.com>
2022-10-21 22:11:08 +0200

    Añadiendo mejoras en la autorización

--- src/app/services/vivienda.service.ts
+++ src/app/services/vivienda.service.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { catchError, map, Observable, throwError } from 'rxjs';
 import { Vivienda } from '../models/vivienda';
 import Swal from 'sweetalert2';
+import { LoginService } from './login.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,11 @@ export class ViviendaService {
   private urlEndpoint: string = 'http://localhost:8080/viviendas';
   private httpHeaders: HttpHeaders = new HttpHeaders({'Content-Type':'application/json'});
 
-  constructor(private httpClient: HttpClient, private router: Router) { }
+  constructor(
+    private httpClient: HttpClient, 
+    private router: Router,
+    private loginService: LoginService
+  ) { }
 
   public getVivienda(id: number): Observable<Vivienda> {
     return this.httpClient.get<Vivienda>(`${this.urlEndpoint}/${id}`).pipe(
@@ -60,7 +65,12 @@ export class ViviendaService {
     formData.append('street', vivienda.street);
     formData.append('bedrooms', vivienda.bedrooms + '');
     formData.append('restrooms', vivienda.restrooms + '');
-    return this.httpClient.post<Vivienda>(this.urlEndpoint + "/photo", formData).pipe(
+    let httpHeaders = new HttpHeaders();
+    let token = this.loginService.token;
+    if (token != null) {
+      httpHeaders = httpHeaders.append('Authorization', 'Bearer' + token);
+    }
+    return this.httpClient.post<Vivienda>(this.urlEndpoint + "/photo", formData, {headers: httpHeaders}).pipe(
       catchError(e => {
         if(this.isNoAutorizado(e)) return throwError(() => new Error('No estás autorizado para añadir esta vivienda'));
         if(e.status === 400) return throwError(() => new Error('Los daton introducidos no son válidos'));
@@ -90,7 +100,12 @@ export class ViviendaService {
     formData.append('street', vivienda.street);
     formData.append('bedrooms', vivienda.bedrooms + '');
     formData.append('restrooms', vivienda.restrooms + '');
-    return this.httpClient.put<Vivienda>(`${this.urlEndpoint}/photo/${vivienda.id}`, formData).pipe(
+    let httpHeaders = new HttpHeaders();
+    let token = this.loginService.token;
+    if (token != null) {
+      httpHeaders = httpHeaders.append('Authorization', 'Bearer ' + token);
+    }
+    return this.httpClient.put<Vivienda>(`${this.urlEndpoint}/photo/${vivienda.id}`, formData, {headers: httpHeaders}).pipe(
       catchError(e => {
         if(this.isNoAutorizado(e)) return throwError(() => new Error('No estás autorizado para actualizar esta vivienda'));
         if(e.status === 400) return throwError(() => new Error('Los datos introducidos no son válidos'));
@@ -101,7 +116,7 @@ export class ViviendaService {
   }
 
   public deleteVivienda(vivienda: Vivienda): Observable<void> {
-    return this.httpClient.delete<void>(`${this.urlEndpoint}/${vivienda.id}`).pipe(
+    return this.httpClient.delete<void>(`${this.urlEndpoint}/${vivienda.id}`, {headers: this.agregarAuthorizationHeader()}).pipe(
       catchError(e => {
         if(this.isNoAutorizado(e)) return throwError(() => new Error('No estás autorizado para eliminar esta vivienda'));
         if(e.status === 400) return throwError(() => new Error('Los datos introducidos no son válidos'));
@@ -112,12 +127,26 @@ export class ViviendaService {
   }
 
   private isNoAutorizado(e): boolean {
-    if(e.status === 401 || e.status === 403) {
+    if (e.status === 401) {
+      Swal.fire('No autorizado', 'No estás autorizado para acceder a este recurso', 'warning');
       this.router.navigate(['/login']);
+      return true;
+    }
+    if (e.status === 403) {
+      Swal.fire('Acceso prohibido', 'No tienes permitido acceder a este recurso', 'warning');
+      this.router.navigate(['/']);
       return true;
     } else {
       return false
     }
+  }
+
+  private agregarAuthorizationHeader() {
+    let token = this.loginService.token;
+    if (token != null) {
+      return this.httpHeaders.append('Authorization', 'Bearer ' + token);
+    }
+    return this.httpHeaders;
   }
 
 }
